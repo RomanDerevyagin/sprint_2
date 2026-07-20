771e3bd8e9483bf914b5bf6ca48381458da791f9
Roman <romanderevyagin@yandex.ru>
2026-07-20 18:41:17 +0300

    sprint_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,15 @@ import model.Apple;
 import model.Food;
 import model.Meat;
 import service.ShoppingCart;
+import static model.constants.Colour.GREEN;
+import static model.constants.Colour.RED;
 
 public class Main {
     public static void main(String[] args) {
         Food[] foods = {
                 new Meat(5, 100),
-                new Apple(10, 50, "red"),
-                new Apple(8, 60, "green")
+                new Apple(10, 50, RED),
+                new Apple(8, 60, GREEN)
         };
         ShoppingCart shoppingCart = new ShoppingCart(foods);
         System.out.println("Сумма без скидки: " + shoppingCart.getTotalWithoutDiscount());
